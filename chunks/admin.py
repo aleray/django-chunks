1a2f5fc8e5573dc6eda19d9b2e8bcf81e7b153a5
Alexandre Leray <alexandre@alexandreleray.com>
2010-02-04 16:57:19 +0100

    Added the Markedit javascript editor to the chunk content field See http://github.com/tstone/jquery-markedit

--- chunks/admin.py
+++ chunks/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
+from django import forms
 from models import Chunk
+from markedit.admin import MarkEditAdmin 
 
-class ChunkAdmin(admin.ModelAdmin):
+
+class ChunkForm(forms.ModelForm):
+    model = Chunk
+    class Media:
+        css = {
+            'all': ('/static/css/markedit/jquery-ui-1.7.2.custom.css',),
+        }
+
+
+class ChunkAdmin(MarkEditAdmin):
   list_display = ('key',)
   search_fields = ('key', 'content')
+  class MarkEdit:
+      fields = [ 'content']
+      options = {        
+        'toolbar': {
+        'backgroundMode': 'light',
+        'layout': 'bold italic | link quote code image | numberlist bulletlist heading line',
+            'buttons' : [],
+            }
+      }
 
-admin.site.register(Chunk, ChunkAdmin)
+admin.site.register(Chunk, ChunkAdmin, form=ChunkForm)
